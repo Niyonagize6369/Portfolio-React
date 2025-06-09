0a0f62dce02aed273ba749fad974a7bd196421c3
Your Name <niyonagizerachel10@email.com>
2025-06-09 09:04:21 +0200

    portfolio-react pushed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,12 @@ const Hero = () => {
       <div className="hero-content">
         <h1>Rachel Niyonagize</h1>
         <h2> I'm Web Developer</h2>
-        {/* <p>i like to do coding</p> */}
+        <p>I'm a passionate web developer dedicated to crafting responsive, 
+          user-friendly websites. I specialize in React,
+           JavaScript, and modern web technologies, 
+          with a strong focus on clean code, performance, and intuitive UI/UX design.
+           My approach blends technical precision with creative problem-solving to deliver impactful digital experiences.
+           I’m always learning and evolving to stay ahead in the ever-changing world of web development.</p>
         {/* <button className="cta-button">View My Work</button> */}
       </div>
       <img src={profilePhoto} alt="profile" className="profile-photo" />
